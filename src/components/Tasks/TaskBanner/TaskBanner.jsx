025b2256116747919d6a5203b9d52db42a5eed90
Raksha Pawar <rakshapawar10.sae@gmail.com>
2022-04-03 22:59:19 +0530

    feat: add Task banner

---
+++ src/components/Tasks/TaskBanner/TaskBanner.jsx
@@ -0,0 +1,13 @@
+import "./TaskBanner.css";
+
+export const TaskBanner = () => {
+  return (
+    <div className="task-banner-container">
+      <div className="card card-basic">
+        <div className="content-container">
+          <h1 className="banner-heading">Welcome Back!</h1>
+        </div>
+      </div>
+    </div>
+  );
+};

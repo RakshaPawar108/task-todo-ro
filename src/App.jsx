915fd260794630b9f3f1ecfc3c5e108c601bbbaa
Raksha Pawar <rakshapawar10.sae@gmail.com>
2022-04-05 18:46:25 +0530

    feat: define a pomodoro route

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import { Footer, Navbar } from "./components";
-import { Home, Tasks } from "./pages";
+import { Home, Pomodoro, Tasks } from "./pages";
 
 function App() {
   return (
@@ -10,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/tasks" element={<Tasks />} />
+        <Route path="/pomodoro" element={<Pomodoro />} />
       </Routes>
       <Footer />
     </div>

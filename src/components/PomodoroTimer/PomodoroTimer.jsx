38a571f1599d7fc1bba56f6dcb438178e99b4f4d
Raksha Pawar <rakshapawar10.sae@gmail.com>
2022-04-05 21:58:17 +0530

    feat: add pomodoro timer component

--- src/components/PomodoroTimer/PomodoroTimer.jsx
+++ src/components/PomodoroTimer/PomodoroTimer.jsx
@@ -1,5 +1,64 @@
+import { useState } from "react";
+import { CountdownCircleTimer } from "react-countdown-circle-timer";
 import "./PomodoroTimer.css";
 
 export const PomodoroTimer = ({ timer }) => {
-  return <div>Timer</div>;
+  const [isPlaying, setIsPlaying] = useState(true);
+  const [key, setKey] = useState(1);
+
+  const renderTimer = ({ remainingTime }) => {
+    const mins = Math.floor((remainingTime % 3600) / 60);
+    const seconds = Math.floor(remainingTime % 60);
+
+    return (
+      <div className="timer-display">
+        <p>
+          {mins} mins: {seconds} secs
+        </p>
+      </div>
+    );
+  };
+  return (
+    <div>
+      <div className="timer">
+        <CountdownCircleTimer
+          isPlaying={isPlaying}
+          duration={timer * 60}
+          colors={["#004777", "#F7B801", "#A30000", "#A30000"]}
+          colorsTime={[timer * 60, (timer * 60) / 4, (timer * 60) / 6, 0]}
+          size={350}
+          key={key}
+        >
+          {({ remainingTime }) => renderTimer({ remainingTime })}
+        </CountdownCircleTimer>
+      </div>
+
+      <div className="timer-controls">
+        <button
+          onClick={() => setIsPlaying(true)}
+          class="button btn-secondary btn-float play-btn"
+          disabled={isPlaying}
+        >
+          <i class="fas fa-play-circle"></i>
+        </button>
+
+        <button
+          onClick={() => setIsPlaying(false)}
+          class="button btn-secondary btn-float pause-btn"
+          disabled={!isPlaying}
+        >
+          <i class="fas fa-pause"></i>
+        </button>
+
+        <div className="reset-button-container">
+          <button
+            onClick={(key) => setKey(key + 1)}
+            class="button btn-secondary"
+          >
+            <i class="fas fa-redo-alt"></i> Reset Timer
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
--- src/components/Tasks/TodoCard/TodoCard.jsx
+++ src/components/Tasks/TodoCard/TodoCard.jsx
@@ -11,30 +11,31 @@ export const TodoCard = ({
   editTodo,
 }) => {
   return (
-    <Link
-      to="/pomodoro"
-      state={{ id: id, name: name, description: description, timer: timer }}
-    >
-      <div className="card card-basic todo-card">
+    <div className="card card-basic todo-card">
+      <Link
+        to="/pomodoro"
+        state={{ id: id, name: name, description: description, timer: timer }}
+      >
         <div className="content-container">
           <h1 className="task-heading">{name}</h1>
           <p className="task-description">{description}</p>
         </div>
-        <div className="action-items-container">
-          <button
-            onClick={() => editTodo(id)}
-            className="button btn-secondary btn-float edit-btn"
-          >
-            <i className="fas fa-pencil-alt"></i>
-          </button>
-          <button
-            onClick={() => deleteTodo(id)}
-            className="button btn-secondary btn-float delete-btn"
-          >
-            <i className="fas fa-trash-alt"></i>
-          </button>
-        </div>
+      </Link>
+
+      <div className="action-items-container">
+        <button
+          onClick={() => editTodo(id)}
+          className="button btn-secondary btn-float edit-btn"
+        >
+          <i className="fas fa-pencil-alt"></i>
+        </button>
+        <button
+          onClick={() => deleteTodo(id)}
+          className="button btn-secondary btn-float delete-btn"
+        >
+          <i className="fas fa-trash-alt"></i>
+        </button>
       </div>
-    </Link>
+    </div>
   );
 };
